7dc82abb77e7c7129f3b63021a8215007f8991d8
prathamnemade <prathameshnemade95@gmail.com>
2018-12-23 10:49:04 +0530

    header part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/components/header/header.component';
+import { MiddleComponent } from './main/components/middle/middle.component';
 
+
+//primeng
+import {RadioButtonModule} from 'primeng/radiobutton';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    MiddleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RadioButtonModule ,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
